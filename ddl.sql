79faf81b97fa528fe0c04b192c76a997b87d1dfb
Paul Wehr <paul@hp.informationsoftworks.com>
2013-10-12 08:52:00 -0400

    inital commit

---
+++ ddl.sql
@@ -0,0 +1,22 @@
+CREATE TABLE recode (
+    startswith integer,
+    original varchar(5),
+    target varchar(5)
+);
+
+
+CREATE TABLE special (
+    dx varchar(5),
+    category varchar(10)
+);
+
+
+CREATE TABLE xacs (
+    dx varchar(5),
+    xed1 numeric,
+    xed2 numeric,
+    xed3 numeric,
+    xed4 numeric,
+    cases numeric
+);
+
