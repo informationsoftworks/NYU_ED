60241c22c5632a41cb392453f7e895ba56d98a75
Paul Wehr <sf_github@informationsoftworks.com>
2019-12-05 06:59:20 -0500

    Unidentified diagnosis codes should default to "unclassified"

--- nyu_ed_query.sql
+++ nyu_ed_query.sql
@@ -7,7 +7,7 @@ select t.*,
 	coalesce(drug, 0) as drug,
 	coalesce(injury, 0) as injury,
 	coalesce(psych, 0) as psych,
-	coalesce(unc, 0) as unc
+	coalesce(unc, 1) as unc
 from my_source_data t -- REPLACE "my_source_data" WITH NAME OF TABLE CONTINING DISCHARGE INFORMATION
 left join nyu_ed_scores n
   on t.dx = n.dx
